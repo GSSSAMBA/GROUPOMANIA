87318c57d80d0bb1390959608a15ad39e6b86433
Gauthier SAMBA SEALE <gssbartender@gmail.com>
2022-12-22 16:01:42 +0100

    commit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 
 mongoose
-    .connect("mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.vmotzji.mongodb.net/?retryWrites=true&w=majority",
+    // .connect("mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.vmotzji.mongodb.net/?retryWrites=true&w=majority",
+    .connect("mongodb+srv://GSSSAMBA:Mongodbb@grouppmania-v-01-deploy.hdpwmir.mongodb.net/test",
 
         {
             useNewUrlParser: true,
